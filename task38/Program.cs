2bfeba6051aba879b9c33bc2d1e3363496e82769
Kate <Kate-pretty@ya.ru>
2022-12-07 23:03:09 +0500

    added files

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,22 +1,41 @@
-﻿void InputArray(int[] array)
+﻿Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
 {
-    for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(1,100);
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
 }
 
-void ReleaseArray(int[] array)
-{
-    int sumEventIndex = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i % 2 != 0)
-            sumEventIndex = sumEventIndex + array[i];
-    }
-    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sumEventIndex}");
-}
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
 
-int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[10];
-InputArray(array);
-Console.WriteLine($"[{string.Join(", ", array)}]");
-ReleaseArray(array);
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + "; ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
