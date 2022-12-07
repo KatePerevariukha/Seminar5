78f6bf4f582a6c9f93e33190b17295421eb6a51f
Kate <Kate-pretty@ya.ru>
2022-12-07 21:51:59 +0500

    added files

---
+++ task34/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100,1000);
+}
+
+void ReleaseArray(int[] array)
+{
+    int countEventNumbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            countEventNumbers++;
+    }
+    Console.WriteLine($"Количество чётных чисел в массиве: {countEventNumbers}");
+}
+
+Console.Write("Введите количество элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
